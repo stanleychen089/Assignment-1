fbba760ad5437c8b2544c184e63856d01ccaaf20
chensta <chensta@OA-S141-02>
2026-01-14 11:02:33 -0500

    BouncingBall Exercise: Added a triangle that bounces upon hitting the screen edge on both the x and y axis

---
+++ Assets/Scripts/BouncingBallExercise.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BouncingBallExercise : MonoBehaviour
+{
+    public float speedX = 0.01f;
+    public float speedY = 0.01f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPos = transform.position;
+        newPos.x += speedX; 
+        newPos.y += speedY;
+        transform.position = newPos;
+
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position); 
+        if(screenPos.x < 0 || screenPos.x > Screen.width)
+        {
+            speedX = speedX * -1; 
+        }
+        if(screenPos.y < 5 || screenPos.y > Screen.height)
+        {
+            speedY = speedY * -1;
+        }
+    }
+}
