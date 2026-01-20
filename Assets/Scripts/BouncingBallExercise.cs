ede2eb290d5e301fab202c33beaf3e7fbc581b45
stanleychen089 <stanleychen089@gmail.com>
2026-01-20 17:26:11 -0500

    Converted bouncing ball exercise into deltaTime and fixed bug due to mismatch between deltaTime and framerate, causing the object to occasionally move off screen and jitter

--- Assets/Scripts/BouncingBallExercise.cs
+++ Assets/Scripts/BouncingBallExercise.cs
@@ -5,28 +5,61 @@ public class BouncingBallExercise : MonoBehaviour
     public float speedX = 0.01f;
     public float speedY = 0.01f;
 
+    Vector2 bottomLeft;
+    Vector2 topRight;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = (Vector2) transform.position + Random.insideUnitCircle * 5; 
+        transform.position = (Vector2) transform.position + Random.insideUnitCircle * 5;
+        bottomLeft = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 newPos = transform.position;
-        newPos.x += speedX; 
-        newPos.y += speedY;
-        transform.position = newPos;
+        newPos.x += speedX * Time.deltaTime; 
+        newPos.y += speedY * Time.deltaTime;
 
+        //check if the object is at the edge of the screen
         Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position); 
-        if(screenPos.x < 0 || screenPos.x > Screen.width)
+        //test for left edge
+        if(screenPos.x < 0)
         {
+            //set our position to be the world space position under pixel 0 in x
+            
+            newPos.x = bottomLeft.x;
             speedX = speedX * -1; 
         }
-        if(screenPos.y < 5 || screenPos.y > Screen.height)
+
+        //test for right edge
+        if (screenPos.x > Screen.width)
         {
+            //set our position to be the world space position under pixel Screen.width in x
+
+            newPos.x = topRight.x; 
+            speedX = speedX * -1;
+        }
+        if(screenPos.y < 0)
+        {
+            //set our position to be the world space position under pixel 0 in y
+            
+            newPos.y = bottomLeft.y;
+            speedY = speedY * -1; 
+        }
+
+        //test for right edge
+        if (screenPos.y > Screen.height)
+        {
+            //set our position to be the world space position under pixel Screen.width in y
+
+            newPos.y = topRight.y; 
             speedY = speedY * -1;
         }
+
+        transform.position = newPos;
+
     }
 }
