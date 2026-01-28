9405f9095df41afdef679c995f7d38bc022391e7
stanleychen089 <chensta@sheridancollege.ca>
2026-01-27 21:19:42 -0500

    Added new script where a duck moves from end of the screen to the other end of the screen, and the speed increases the closer the moon is

---
+++ Assets/Scripts/PanickDucks.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PanickDucks : MonoBehaviour
+{
+
+    //a public reference to moon's position 
+    public Transform moonPos;
+
+    public float duckSpeed;
+
+    //vectors used to check for screen border 
+    Vector2 bottomLeft;
+    Vector2 topRight;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bottomLeft = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //a vector to obtain the moon's x position 
+        Vector2 moonDistance = moonPos.position;
+
+        //speed increases when moon is closer to centre 
+        duckSpeed = (9 + moonDistance.x);
+
+        //vector that obtains duck's x position to move it 
+        Vector2 newPos = transform.position;
+        newPos.x += duckSpeed * Time.deltaTime*2;
+
+         //code from 2-4 from readings, which checks of object is at the edge of the screen
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        if(screenPos.x > Screen.width +1)
+        {
+            newPos.x = bottomLeft.x - 1;
+            
+        }
+
+        transform.position = newPos;
+
+    }
+}
